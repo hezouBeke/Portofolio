9d0e6729c2eda73196f14ac462fd8658f29ca559
hezou <hezoubeke18@gmail.com>
2024-03-25 09:44:08 +0000

    dashboard initializing with Acceuil section

--- portofolio_project/src/app/app.component.ts
+++ portofolio_project/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet
+  imports: [
+    RouterOutlet ,
+    DashboardComponent
   ],
+   
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'portofolio_project';
 }
--- portofolio_project/src/app/app.routes.ts
+++ portofolio_project/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    { path: '',   redirectTo : 'dashboard' , component: DashboardComponent }, 
+
+];
